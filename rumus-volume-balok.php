b231a91823a36bf0c84b538065aa72985daa3722
baronnalonso65 <baronnalonso65@gmail.com>
2025-08-20 21:45:04 +0700

    volume balok

---
+++ rumus-volume-balok.php
@@ -0,0 +1,10 @@
+<?php
+echo "<h3>Rumus Volume Balok</h3>";
+$p = 12;
+$l = 5;
+$t = 6;
+$volume = $p * $l * $t;
+
+echo "Panjang = $p, Lebar = $l, Tinggi = $t <br>";
+echo "Volume = P × L × T = $p × $l × $t = $volume";
+?>
