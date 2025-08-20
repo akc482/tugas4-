4c1c687747e5fcf064ad73587d31247f7f4651a6
baronnalonso65 <baronnalonso65@gmail.com>
2025-08-20 21:38:45 +0700

    rumus konversi

---
+++ Rumus-Konversi-Celsius-keFahrenheit.php
@@ -0,0 +1,8 @@
+<?php
+echo "<h3>Rumus Konversi Celsius ke Fahrenheit</h3>";
+$celsius = 30;
+$fahrenheit = ($celsius * 9/5) + 32;
+
+echo "Suhu = $celsius °C <br>";
+echo "Konversi = (C × 9/5) + 32 = ($celsius × 9/5) + 32 = $fahrenheit °F";
+?>
